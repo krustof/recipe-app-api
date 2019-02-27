474cee1f46fbf0c101568db614580e0fc8082256
Geoff Pollard <gpollard@tp.local>
2019-02-27 10:45:16 +0800

    fix linting

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -59,7 +59,7 @@ class PrivateTagsAPITests(TestCase):
         tag = Tag.objects.create(user=self.user, name='Comfort Food')
 
         res = self.client.get(TAGS_URL)
-        
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
